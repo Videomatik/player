a9ae5d539a4d71ed338e06fb7edf5fb0a763f02b
Renan Lamas <renanblamas@gmail.com>
2022-10-27 16:46:46 -0300

    Create basic Videomatik Player class and option

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-const addNumber = (number) => {
-  return (number + 2)
-}
+class VideomatikPlayer {
+  constructor(containerSelector, options) {
+    const { 
+      __playerURL, 
+      apiKey, 
+      templateId, 
+      compositionId = "default",
+      maxHeight = '100%',
+      maxWidth = '100%',
+    } = options
+    const container = document.querySelector(containerSelector)
+    const iframe = document.createElement('iframe')
+    iframe.setAttribute("src", `${__playerURL}/v1?templateId=${templateId}&apiKey=${apiKey}&compositionId=${compositionId}`);
 
-module.exports = addNumber
+    iframe.width =  maxWidth
+    iframe.height = maxHeight
+    container.appendChild(iframe)
+  }
+}
+module.exports = VideomatikPlayer
