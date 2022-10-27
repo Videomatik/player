80296d2d4eaf28204fd80547f827e3dbcb48431e
Renan Lamas <renanblamas@gmail.com>
2022-10-27 16:54:54 -0300

    Add Eslint and fix errors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 class VideomatikPlayer {
   constructor(containerSelector, options) {
-    const { 
-      __playerURL, 
-      apiKey, 
-      templateId, 
-      compositionId = "default",
+    const {
+      __playerURL,
+      apiKey,
+      templateId,
+      compositionId = 'default',
       maxHeight = '100%',
       maxWidth = '100%',
-    } = options
-    const container = document.querySelector(containerSelector)
-    const iframe = document.createElement('iframe')
-    iframe.setAttribute("src", `${__playerURL}/v1?templateId=${templateId}&apiKey=${apiKey}&compositionId=${compositionId}`);
+    } = options;
+    const container = document.querySelector(containerSelector);
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', `${__playerURL}/v1?templateId=${templateId}&apiKey=${apiKey}&compositionId=${compositionId}`);
 
-    iframe.width =  maxWidth
-    iframe.height = maxHeight
-    container.appendChild(iframe)
+    iframe.width = maxWidth;
+    iframe.height = maxHeight;
+    container.appendChild(iframe);
   }
 }
-module.exports = VideomatikPlayer
+module.exports = VideomatikPlayer;
